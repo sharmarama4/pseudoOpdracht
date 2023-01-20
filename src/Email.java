1bb7e93577f806da5f18ae89eda23a228c460346
sharmarama4 <sharmarama418@gmail.com>
2023-01-20 14:25:27 +0100

    Commited by Rama

---
+++ src/Email.java
@@ -0,0 +1,73 @@
+public class Email{
+    private String to;
+    private String from;
+    private String cc;
+    private String emailText;
+    private String subject;
+
+    public Email() {
+    }
+
+    public Email(String to, String from, String cc, String emailText, String subject) {
+        this.to = to;
+        this.from = from;
+        this.cc = cc;
+        this.emailText = emailText;
+        this.subject = subject;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+
+    public String getEmailText() {
+        return emailText;
+    }
+
+    public void setEmailText(String emailText) {
+        this.emailText = emailText;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "to='" + to + '\'' +
+                ", from='" + from + '\'' +
+                ", cc='" + cc + '\'' +
+                ", emailText='" + emailText + '\'' +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
+
+
+}
+
+
---
+++ src/EmailApp.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EmailApp {
+    public static void main(String[] args) {
+        // Get the list of emails
+        List<Email> emails=new ArrayList<>();
+        emails.add(new Email(" ","info@parkshark.com","","Thanks for Promo","Promo"));
+        emails.add(new Email(" ","info@parkshark.com","","I want to do advertising","My books advertising"));
+        emails.add(new Email(" ","info@parkshark.com","","This is my business proposal","About my Company's"));
+        emails.add(new Email(" ","info@parkshark.com","","This is my 2nd proposal","To sales my house"));
+        emails.add(new Email(" ","info@parkshark.com","","Find below my Cv","About my cv"));
+        emails.add(new Email(" ","info@parkshark.com","","I want to learn english","To get information about my java course"));
+        emails.add(new Email(" ","info@parkshark.com","","Can i make an appointment?","about info"));
+        emails.add(new Email(" ","info@parkshark.com","","I want to follow this Course","Dutch course"));
+        emails.add(new Email(" ","info@parkshark.com","","This is my Cv","About job"));
+        emails.add(new Email(" ","info@parkshark.com","","Cv","About junior job "));
+        emails.add(new Email(" ","info@parkshark.com","","can you correct my Cv please?","To correct my cv"));
+        emails.add(new Email(" ","info@parkshark.com","","Cv of java developer","To "));
+
+        //declare the particular list to store the mails
+        List<Email>spamList=new ArrayList<>();
+        List<Email>proposalList=new ArrayList<>();
+        List<Email>recepList=new ArrayList<>();
+        List<Email>cvList=new ArrayList<>();
+
+        //select the emails from the list to store the particular list
+           emails.forEach(em->{
+            if(em.getEmailText().contains("Promo")||em.getEmailText().contains("advertising")){
+                // forward mail /send em
+                em.setTo("spam@parkshark.com");
+                //delete this email
+                spamList.remove(em);
+                //add to the spamList
+                spamList.add(em);
+            }
+            else if(em.getEmailText().contains("proposal")){
+                //forward mail
+                em.setTo("sales@parkshark.com");
+                //delete this email
+                proposalList.remove(em);
+
+                //add to the list for the count
+                proposalList.add(em);
+
+            }
+            else if(em.getEmailText().contains("Cv")){
+                em.setTo("recruitment@parkshark.com");
+                //delete this email
+                cvList.remove(em);
+                // add to the list
+                cvList.add(em);
+
+            }
+            else{
+                em.setTo("reception@parkshark.com");
+                recepList.remove(em);
+                recepList.add(em);
+            }
+           });
+           //display total amount of emails sent
+           System.out.println("We have sorted "+emails.size()+" mails :");
+        //display total amount of emails sent to recruitment
+        System.out.println(cvList.size()+" to recruitment");
+        //display total amount of emails sent to spam
+        System.out.println(spamList.size()+" to spam");
+        //display total amount of emails sent to sales or advertisement
+        System.out.println(proposalList.size()+" to sales");
+        //display total amount of emails sent to reception
+        System.out.println(recepList.size()+" to reception");
+
+
+
+    }
+}
+
+
+
